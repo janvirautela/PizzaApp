8806500a8c0f7944284e066f6c3e0a5e55c7bd1f
janvirautela <77125120+janvirautela@users.noreply.github.com>
2021-01-09 19:11:49 -0500

    first commit

---
+++ IfStatements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool pizzaIsHealthy;
+    int choice;
+    printf("Do you think pizza is healthy? Type 1 for true and 0 for false: ");
+    scanf("%d", &choice); //%d is preferred here 
+    pizzaIdHealthy = choice;
+    if(pizzaIsHealthy)
+    {
+        printf("Welcome to my Pizza App\n")
+    }
+}
